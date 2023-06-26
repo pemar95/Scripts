f7c2b7e53bd086c6a65b9827647671c8636a368c
Error404 <65098629+error404eu@users.noreply.github.com>
2023-06-27 00:19:01 +0200

    Update MainMenu.ps1

--- MainMenu.ps1
+++ MainMenu.ps1
@@ -24,12 +24,22 @@ function Show-Menu {
 # Define individual script functions
 function Run-Script0 {
     Write-Host "Clearing Powershell history before exiting..."
-    Remove-Item (Get-PSreadlineOption).HistorySavePath
+    Remove-Item (Get-PSReadlineOption).HistorySavePath
     exit
 }
 
 function Run-Script1 {   
     powershell -ExecutionPolicy Bypass -Command "Invoke-Expression ((Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/error404eu/Scripts/main/WLAN-PSW-Finder.ps1').Content);pause"
     Press-AnyKey
+}
+
+# Helper function to pause execution and wait for user input
+function Press-AnyKey {
+    Write-Host "Press any key to continue..."
+    $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+}
+
+# Main loop to display the menu and process user input
+while ($true) {
     Show-Menu
 }
