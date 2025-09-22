efd0b462eb93dc4bdf22074c81943ee3e2705561
Pemar <65098629+pemar95@users.noreply.github.com>
2025-09-22 22:58:32 +0200

    Update Port-Discovery.ps1

--- Port-Discovery.ps1
+++ Port-Discovery.ps1
@@ -1,3 +1,2 @@
 ([Net.NetworkInformation.IPGlobalProperties]::GetIPGlobalProperties()).GetActiveTcpListeners() |
-    Where-Object { $_.Address -eq [System.Net.IPAddress]::Any } |
-    Select-Object Port
+    Where-Object { $_.Address -eq [System.Net.IPAddress]::Any }
